b184244b45fe592119c67a10286d7510832d7683
vvmsecured <iva9Yatoj>
2018-04-09 19:18:37 +0500

    isanmon

--- js/background.js
+++ js/background.js
@@ -1,11 +1,34 @@
 class BG {
 
     constructor(){
+        this.count = 0;
+        this.timestart = Math.floor(Date.now() / 1000);
+        this.avg_min = 0;
+    }
 
+    snitch(){
+        this.count++;
+        this.avg_min = Math.floor((60 * this.count / (Math.floor(Date.now() / 1000) - this.timestart)) * 100) / 100;
+        this.interval = Math.floor(((Math.floor(Date.now() / 1000) - this.timestart) / this.count) * 100) / 100;
+        chrome.browserAction.setBadgeText({
+            text: this.interval.toString()
+        });
+        chrome.browserAction.setBadgeBackgroundColor({
+            color: 'green'
+        });
+    }
+
+    tick(){
+        this.count = 0;
+        this.timestart = Math.floor(Date.now() / 1000);
+        this.avg_min = 0;
     }
 
     static init(){
         let bg = new BG();
+        let ionic = new Ionic('http://n.isan.pro:7070');
+        ionic.boom('snitch_open', () => {bg.snitch();});
+        setInterval(() => {bg.tick();}, 5 * 60 * 1000)
     }
 }
 
